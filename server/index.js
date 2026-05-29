cda147d9ee84133cab47e7cdc196cce45b1eb492
fffggdvfdcg-arch <___github>
2026-05-30 01:47:20 +0600

    fix cors

--- server/index.js
+++ server/index.js
@@ -11,7 +11,13 @@ const cruiseRoutes = require("./routes/cruiseRoutes");
 
 const app = express();
 
-app.use(cors({ origin: process.env.CLIENT_URL || "http://localhost:5173", credentials: true }));
+app.use(cors({
+  origin: [
+    "http://localhost:5173",
+    "https://travelpro-world.netlify.app"
+  ],
+  credentials: true
+}));
 app.use(express.json());
 
 // API routes
